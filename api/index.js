30b15a4591707917ae17d96a0b1098fe2658d55f
PonomareVlad <Git@PonomareVlad.ru>
2020-11-17 03:55:48 +0500

    Output stringified Request object

--- api/index.js
+++ api/index.js
@@ -1,3 +1,20 @@
 export default async (req, res) => {
-    res.json(req)
+    res.json(simpleStringify(req))
+}
+
+function simpleStringify(object) {
+    var simpleObject = {};
+    for (var prop in object) {
+        if (!object.hasOwnProperty(prop)) {
+            continue;
+        }
+        if (typeof (object[prop]) == 'object') {
+            continue;
+        }
+        if (typeof (object[prop]) == 'function') {
+            continue;
+        }
+        simpleObject[prop] = object[prop];
+    }
+    return JSON.stringify(simpleObject); // returns cleaned up JSON
 }
