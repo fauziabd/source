5b9c972341701733067169da8320cfc2692958b3
PonomareVlad <Git@PonomareVlad.ru>
2022-05-02 18:55:48 +0500

    Refactored template with .mjs functions support

--- index.mjs
+++
@@ -1,9 +0,0 @@
-export default async (req, res) => {
-    res.json({
-        body: req.body,
-        query: req.query,
-        cookies: req.cookies,
-        headers: req.headers,
-        versions: process.versions
-    })
-}
