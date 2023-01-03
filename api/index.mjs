bc3030027bfc9cbf3a71b69f82da3174ecad3107
Fauzi <13282094+fauziabd@users.noreply.github.com>
2023-01-03 09:15:07 +0700

    puppeteer

--- api/index.mjs
+++ api/index.mjs
@@ -1,8 +1,30 @@
-export default async ({body, query, cookies, headers}, {json}) => json({
-    body,
-    query,
-    cookies,
-    headers,
-    env: process.env,
-    versions: process.versions
-})
+import edgeChromium from 'chrome-aws-lambda'
+
+// Importing Puppeteer core as default otherwise
+// it won't function correctly with "launch()"
+import puppeteer from 'puppeteer-core'
+
+// You may want to change this if you're developing
+// on a platform different from macOS.
+// See https://github.com/vercel/og-image for a more resilient
+// system-agnostic options for Puppeteeer.
+const LOCAL_CHROME_EXECUTABLE = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+
+export default async function (req, res) {
+  // Edge executable will return an empty string locally.
+  const executablePath = await edgeChromium.executablePath || LOCAL_CHROME_EXECUTABLE
+  
+  const browser = await puppeteer.launch({
+    executablePath,
+    args: edgeChromium.args,
+    headless: false,
+  })
+  
+  const page = await browser.newPage()
+  await page.goto('https://github.com', {waitUntil: 'domcontentloaded'})
+  
+  res.header({'content-type': 'text:plain'})
+  res.send(await page.content())
+
+  await browser.close()
+}
